c035e5629b1674a870fd5256ad9dd722a61c7bc5
Dmitriy Timofeev <28dngn@gmail.com>
2026-02-11 14:50:54 +0300

    задание готово

--- insert_data.sql
+++ insert_data.sql
@@ -1,23 +1,77 @@
-INSERT INTO genres (name_genre)
-VALUES ('Pop'), ('Rock'), ('Jazz');
+INSERT INTO genres (genre_name)
+VALUES
+    ('Pop'),
+    ('Rock'),
+    ('Jazz');
 
-INSERT INTO musicians (name_musician)
-VALUES ('Michael Jackson'), ('Rihanna'), ('Metallica'), ('Karen Souza'), ('Diana Krall');
+INSERT INTO musicians (musician_name)
+VALUES
+    ('Michael Jackson'),
+    ('Rihanna'),
+    ('Metallica'),
+    ('Karen Souza'), 
+    ('Diana Krall');
 
 INSERT INTO genre_musician (genre_id, musician_id)
-VALUES (1, 1), (1, 2), (2, 3), (3, 4), (3, 5);
+VALUES
+    (1, 1),
+    (1, 2),
+    (3, 2),
+    (2, 3),
+    (3, 4),
+    (3, 5);
 
-INSERT INTO albums (title_album, release_date)
-VALUES ('Bad', DATE '1987-08-25'), ('Dangerous', DATE '1920-11-26'), ('Loud', DATE '2019-01-01'), ('Master of Puppets', DATE '1986-02-12'), ('Hotel Souza', DATE '2019-01-01'), ('The Look of Love', DATE '2020-01-01');
+INSERT INTO albums (album_name, release_date)
+VALUES
+    ('Bad', DATE '1987-08-25'),
+    ('Dangerous', DATE '2020-11-26'),
+    ('Loud', DATE '2019-01-01'),
+    ('Master of Puppets', DATE '1986-02-12'),
+    ('Hotel Souza', DATE '2019-01-01'),
+    ('The Look of Love', DATE '2019-01-01');
 
-INSERT INTO musician_album (musician_id, album_id)
-VALUES (1, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6);
+INSERT INTO discography (musician_id, album_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 3),
+    (3, 4),
+    (4, 5),
+    (5, 6);
 
-INSERT INTO tracks (title_track, length_track, album_id)
-VALUES ('Bad', 247, 1), ('Dirty Diana', 281, 1), ('Jam', 339, 2), ('S&M', 243, 3), ('Fading', 199, 3), ('Battery', 288, 4), ('My Paris', 226, 5), ('Dindi', 239, 5), ('Cry Me a River', 305, 6), ('Love Letters', 295, 6);
+INSERT INTO tracks (track_name, track_size, album_id)
+VALUES
+    ('Bad', 247, 1),
+    ('Dirty Diana', 281, 1),
+    ('Jam', 339, 2),
+    ('S&M', 243, 3),
+    ('Fading', 199, 3),
+    ('Battery', 288, 4),
+    ('My Paris', 226, 5),
+    ('Dindi', 239, 5),
+    ('Cry Me a River', 305, 6),
+    ('Love Letters', 295, 6);
 
-INSERT INTO collections (title_collection, release_date)
-VALUES ('Best-1', DATE '2018-01-07'), ('Best-2', DATE '2020-12-24'), ('Pop-Rock', DATE '2026-02-09'), ('Best Jazz', DATE '2020-02-09');
+INSERT INTO collections (collection_name, release_date)
+VALUES
+    ('Best-1', DATE '2018-01-07'),
+    ('Best-2', DATE '2020-12-24'),
+    ('Pop-Rock', DATE '2026-02-09'),
+    ('Best Jazz', DATE '2020-02-09');
 
 INSERT INTO track_collection (track_id, collection_id)
-VALUES (1, 1), (3, 1), (5, 1), (2, 2), (3, 2), (4, 2), (1, 3), (2, 3), (5, 3), (2, 3), (6, 3), (7, 4), (8, 4), (9, 4), (10, 4);
+VALUES
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (2, 2),
+    (3, 2),
+    (4, 2),
+    (1, 3),
+    (2, 3),
+    (5, 3),
+    (2, 3),
+    (6, 3),
+    (7, 4),
+    (8, 4),
+    (10, 4);
--- select_data.sql
+++ select_data.sql
@@ -1,38 +1,121 @@
--- Название и продолжительность самого длительного трека
-SELECT
-title_track,
-CONCAT(
-    FLOOR(length_track / 60),
-    ':',
-    LPAD((length_track % 60)::text, 2, '0')
-  ) AS length_track
-FROM tracks
-WHERE length_track = (SELECT MAX(length_track) FROM tracks);
+  -- ЗАДАНИЕ 2
 
--- Название треков, продолжительность которых не менее 3,5 минут
+-- 1. Название и продолжительность самого длительного трека
 SELECT
-title_track,
-CONCAT(
-    FLOOR(length_track / 60),
-    ':',
-    LPAD((length_track % 60)::text, 2, '0')
-  ) AS length_track
+    track_name,
+    CONCAT(
+        FLOOR(track_size / 60),
+        ':',
+        LPAD((track_size % 60)::text, 2, '0')
+      ) AS track_size
 FROM tracks
-WHERE length_track >= 210
-ORDER BY length_track;
+WHERE track_size = (SELECT MAX(track_size) FROM tracks);
 
--- Названия сборников, вышедших в период с 2018 по 2020 год включительно
-SELECT title_collection, release_date rd
+-- 2. Название треков, продолжительность которых не менее 3,5 минут
+SELECT
+  track_name,
+  CONCAT(
+      FLOOR(track_size / 60),
+      ':',
+      LPAD((track_size % 60)::text, 2, '0')
+    ) AS track_size
+FROM tracks
+WHERE track_size >= 210
+ORDER BY track_size;
+
+-- 3. Названия сборников, вышедших в период с 2018 по 2020 год включительно
+SELECT collection_name, release_date
 FROM collections
 WHERE release_date BETWEEN DATE '2018-01-01' AND DATE '2020-12-31';
 
--- Исполнители, чьё имя состоит из одного слова
-SELECT name_musician
+-- 4. Исполнители, чьё имя состоит из одного слова
+SELECT musician_name
 FROM musicians
-WHERE name_musician LIKE '% %';
+WHERE musician_name LIKE '% %';
 
--- Название треков, которые содержат слово «мой» или «my»
-SELECT title_track
+-- 5. Название треков, которые содержат слово «мой» или «my»
+SELECT track_name
 FROM tracks
-WHERE title_track ILIKE '%my%' 
-   OR title_track ILIKE '%мой%';
+WHERE track_name ILIKE '%my%' 
+  OR track_name ILIKE '%мой%';
+
+
+   -- ЗАДАНИЕ 3
+
+-- 1. Количество исполнителей в каждом жанре
+SELECT g.genre_name, COUNT(gm.musician_id) AS musician_total
+FROM genres AS g
+LEFT JOIN genre_musician AS gm ON gm.genre_id = g.genre_id
+GROUP BY g.genre_name;
+
+-- 2. Количество треков в альбомах 2019–2020
+SELECT COUNT(t.track_id) AS tracks_total
+FROM tracks AS t
+LEFT JOIN albums AS a ON t.album_id = a.album_id
+WHERE a.release_date BETWEEN DATE '2019-01-01' AND DATE '2020-12-31';
+
+-- 3. Средняя продолжительность треков по каждому альбому
+SELECT
+    a.album_name,
+    CONCAT(
+        FLOOR(AVG(t.track_size) / 60),
+        ':',
+        TO_CHAR(FLOOR(AVG(t.track_size) % 60), 'FM00')
+      ) AS mean_size
+FROM albums a 
+LEFT JOIN tracks t ON t.album_id = a.album_id
+GROUP BY a.album_name
+ORDER BY mean_size DESC;
+
+-- 4. Все исполнители, которые не выпустили альбомы в 2020 году
+SELECT m.musician_name
+FROM musicians m
+LEFT JOIN discography d ON d.musician_id = m.musician_id
+LEFT JOIN albums a ON a.album_id = d.album_id
+WHERE EXTRACT(YEAR FROM a.release_date) != 2020;
+
+
+   -- ЗАДАНИЕ 4
+
+-- 1. Названия альбомов, в которых присутствуют исполнители более чем одного жанра
+SELECT a.album_name
+FROM albums a
+JOIN discography d ON d.album_id = a.album_id
+JOIN musicians m ON m.musician_id = d.musician_id
+WHERE m.musician_id IN (
+    SELECT gm.musician_id
+    FROM genre_musician gm
+    GROUP BY gm.musician_id
+    HAVING COUNT(gm.genre_id) > 1
+);
+
+-- 2. Наименования треков, которые не входят в сборники
+SELECT t.track_name
+FROM tracks t
+WHERE t.track_id NOT IN (
+    SELECT tc.track_id
+    FROM track_collection tc
+);
+
+-- 3. Исполнитель или исполнители, написавшие самый короткий по продолжительности трек
+SELECT m.musician_name  
+FROM musicians m
+JOIN discography d ON d.musician_id = m.musician_id
+JOIN albums a ON a.album_id = d.album_id
+JOIN tracks t ON t.album_id = a.album_id
+WHERE t.track_size = (
+    SELECT MIN(track_size)
+    FROM tracks
+);
+
+-- 4. Названия альбомов, содержащих наименьшее количество треков
+SELECT a.album_name, COUNT(t.track_id) AS track_count
+FROM albums a
+JOIN tracks t ON t.album_id = a.album_id
+GROUP BY a.album_id, a.album_name
+HAVING COUNT(t.track_id) <= ALL (
+    SELECT COUNT(t2.track_id)
+    FROM albums a2
+    JOIN tracks t2 ON t2.album_id = a2.album_id
+    GROUP BY a2.album_id
+);
